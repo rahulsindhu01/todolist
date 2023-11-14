6ee04ad67d4fc9a6ef8a20301102f9401d282219
Rahul.dev <107878091+rahulsindhu01@users.noreply.github.com>
2023-11-14 11:09:59 +0530

    Script for pomodoro

--- script.js
+++ script.js
@@ -41,3 +41,55 @@ function showTask(){
     ListContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+
+
+
+
+
+let timer;
+let minutes = 50;
+let seconds = 0;
+let timerStarted = false;
+
+function startTimer() {
+  if (!timerStarted) {
+    timerStarted = true;
+    timer = setInterval(updateTimer, 1000);
+  }
+}
+
+function pauseTimer() {
+  clearInterval(timer);
+  timerStarted = false;
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  timerStarted = false;
+  minutes = 50;
+  seconds = 0;
+  updateDisplay();
+}
+
+function updateTimer() {
+  if (minutes === 0 && seconds === 0) {
+    clearInterval(timer);
+    timerStarted = false;
+    alert("Pomodoro completed!");
+  } else {
+    if (seconds === 0) {
+      minutes--;
+      seconds = 59;
+    } else {
+      seconds--;
+    }
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  document.getElementById('timer').innerText = `${formattedMinutes}:${formattedSeconds}`;
+}
